1a2f129a703731a4296f9472da6c76bfcc84995e
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-05-08 19:48:04 -0300

    Add CORS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express  from 'express';
 import employeesRoute from './routes/employes.routes.js'
 import indexRoute from './routes/index.routes.js';
+import cors from 'cors'
 import {PORT} from './config.js';
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 
 app.use(indexRoute);
